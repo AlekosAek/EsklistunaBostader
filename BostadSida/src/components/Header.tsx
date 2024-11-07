6966966609a1499d797bd0102c3981dac4050a84
vw rabbit <vkutov@gmail.com>
2024-11-07 10:49:17 +0100

    register and login links

--- BostadSida/src/components/Header.tsx
+++ BostadSida/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { NavLink, useLocation } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+
 import "./Header.css"
 export default function Header() {
   const location = useLocation(); // Get the current route location
@@ -9,10 +11,14 @@ export default function Header() {
        <header className="userLogin">
         <aside></aside>
         <aside >
+          <Link to="/register">
            Registrera dig 
+          </Link>
         </aside>
         <aside>
+        <Link to="/login">
           Mina sidor - Logga in
+        </Link>
         </aside>
        </header>
     </>
