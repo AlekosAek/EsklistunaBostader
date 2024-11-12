9ab4030ea84a85ed18b2d344431af525370cf081
vw rabbit <vkutov@gmail.com>
2024-11-12 15:33:43 +0100

    fixed facebook and instagram

--- BostadSida/src/router.tsx
+++ BostadSida/src/router.tsx
@@ -1,29 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./components/App.tsx";
-import { Home } from "./pages/Home";
-// import { NotFound } from "./pages/NotFound.tsx";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "./App";
+import HomePage from "./Pages/HomePage";
+import NotFound from "./Pages/NotFoundPage";
 
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} errorElement={<NotFound />}>
+        <Route index element={<HomePage />} />
+        <Route path="/not-found" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </>
+  )
+);
 
-
-
-export const router = createBrowserRouter([
-  {
-    element: <App />,
-    path: "/",
-    children: [
-      // home page
-      {
-        index: true,
-        element: <Home />,
-      },   
-      {
-        path: "/contact",
-        element: <Home />,
-      },
-      {
-        path: "/about",
-        element: <Home />,
-      },
-     ],
-   },
-]);
