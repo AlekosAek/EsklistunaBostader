c5994475711f0dbafae233744ddc1036b9a9fea5
vw rabbit <vkutov@gmail.com>
2024-11-15 13:39:18 +0100

    added  left menu

--- EsklistunaBostad/src/Pages/OmOss.tsx
+++ EsklistunaBostad/src/Pages/OmOss.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import "./css/OmOss.css";
 
 function OmOss() {
   return (
-    <div>
-      
-    </div>
+    <section className = "about">
+       <menu className = "menu">
+        <aside >
+        </aside>
+         <aside>
+             <Link to="https://www.bostadeskilstuna.se/CM/Templates/Article/general.aspx?cmguid=b57ed27a-9783-48de-b4f9-f05569243779">För fastighetsägare</Link>
+         </aside>
+           <hr />
+         <aside>
+             <Link to="https://www.bostadeskilstuna.se/CM/Templates/Article/general.aspx?cmguid=ee3c09a0-9f21-4895-bdc8-81f5cd352b1b">Vår Personuppgiftspolicy</Link>
+         </aside>
+           <hr />
+       </menu>
+    </section>
   )
 }
 
