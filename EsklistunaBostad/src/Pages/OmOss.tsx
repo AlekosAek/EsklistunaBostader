6d4c1983b740c95fc69fadeaf8a0ad8d3c63a522
vw rabbit <vkutov@gmail.com>
2024-11-17 14:45:11 +0100

    fixed a gallery

--- EsklistunaBostad/src/Pages/OmOss.tsx
+++ EsklistunaBostad/src/Pages/OmOss.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import "./css/OmOss.css";
 import "./css/Esklistuna.css";
+import first from "../assets/Peter.jpg";
+import second from "../assets/Linda.jpg";
+import third from "../assets/Viktor.jpg";
+import fourth from "../assets/William.jpg";
+import fifth from "../assets/Marielle.jpg";
+import sixth from "../assets/Camilla.jpg";
 
 function OmOss() {
   return (
@@ -27,14 +33,13 @@ function OmOss() {
 
         Vi har specialiserat oss på att marknadsföra, informera, förmedla och hantera bostadsutbud och -sökande. Vi samarbetar med fastighetsägare, byggföretag, Eskilstuna kommun och andra bostadsaktörer för att på ett effektivt sätt erbjuda intresserade ett nytt boende.
 
-        Bostad Eskilstuna drivs av Bostad Västerås AB som erbjuder liknande tjänster i Västerås.  
+        Bostad Eskilstuna drivs av <Link to="https://www.bostadeskilstuna.se/CM/Templates/Article/general.aspx?cmguid=b57ed27a-9783-48de-b4f9-f05569243779">Bostad Västerås AB</Link> som erbjuder liknande tjänster i Västerås.  
        </p>
         <h2>
         Vi är till för dig
         </h2>
         <p>
         Undrar du över något, är du alltid välkommen att fråga oss. Vi kan det mesta och litet till! Funderar du på att byta bostad eller bara vill informera dig om bostadsmarknaden för studenter i Eskilstuna hör du av dig till oss via e-mail, telefon eller våra sociala medier.
-        
         </p>
         
         <h2>
@@ -43,7 +48,41 @@ function OmOss() {
         <p>
         Här kan du läsa vilken hjälp du kan få.
         </p>
-        Läs mer
+        <p>
+            <Link to="https://www.bostadeskilstuna.se/CM/Templates/Article/general.aspx?cmguid=b57ed27a-9783-48de-b4f9-f05569243779">Läs mer</Link>
+        </p>
+           
+       <section className='gallery'>
+       
+          <figure>
+             <img src={first} alt= "Peter Odeheim"  />
+            <figcaption>Peter Odeheim</figcaption>
+          </figure>
+          <figure>
+             <img src={second} alt= "Linda Dahlgren"  />
+              <figcaption>Linda Dahlgren</figcaption>
+
+          </figure>
+          <figure>
+             <img src={third} alt="Viktor Persson Bengtsson"  />
+              <figcaption>Viktor Persson Bengtsson</figcaption>
+          </figure>
+          <figure>
+             <img src={fourth} alt="William Håkansson
+"  />
+              <figcaption>William Håkansson</figcaption>
+          </figure>
+          <figure>
+             <img src={fifth} alt="Marielle Mokvist"  />
+              <figcaption>Marielle Mokvist</figcaption>
+          </figure>
+          <figure>
+             <img src={sixth} alt="Camilla Toth"  />
+               <figcaption>Camilla Toth</figcaption>
+
+          </figure>
+       </section>
+        
        </article>
     </section>
   )
